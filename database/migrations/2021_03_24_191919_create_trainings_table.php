346d0d5865c28b4e7ef6ce3d71be35ba864f2932
Jwan <jwanaalfatla1999@gmail.com>
2021-03-24 22:54:23 +0300

    first commit

---
+++ database/migrations/2021_03_24_191919_create_trainings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trainings', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('type');
+            $table->string('date');
+            $table->string('period');
+            $table->string('trainer');
+            $table->boolean('in_person');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trainings');
+    }
+}
