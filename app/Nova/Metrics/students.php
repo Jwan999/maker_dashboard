f1ece21bf17fbd2661ad052c67730c8cf3291868
Jwan <jwanaalfatla1999@gmail.com>
2021-05-31 11:52:48 +0300

    First commnit

--- app/Nova/Metrics/students.php
+++ app/Nova/Metrics/students.php
@@ -11,7 +11,7 @@ class students extends Value
     /**
      * Calculate the value of the metric.
      *
-     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @param \Laravel\Nova\Http\Requests\NovaRequest $request
      * @return mixed
      */
     public function calculate(NovaRequest $request)
@@ -27,11 +27,6 @@ class students extends Value
     public function ranges()
     {
         return [
-            30 => __('30 Days'),
-            60 => __('60 Days'),
-            365 => __('365 Days'),
-            'TODAY' => __('Today'),
-            'MTD' => __('Month To Date'),
             'QTD' => __('Quarter To Date'),
             'YTD' => __('Year To Date'),
         ];
