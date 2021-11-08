07eac6d1d232317242ad0e4f6a030e0f631af008
Jwan <jwanaalfatla1999@gmail.com>
2021-11-08 15:51:03 +0300

    Added icons to the side menu

--- database/migrations/2021_11_08_123923_change_trainings_table_column.php
+++ database/migrations/2021_11_08_123923_change_trainings_table_column.php
@@ -14,6 +14,7 @@ class ChangeTrainingsTableColumn extends Migration
     public function up()
     {
         Schema::table('trainings', function (Blueprint $table) {
+            $table->dropForeign('trainings_trainer_id_foreign');
             $table->dropColumn('trainer_id');
         });
     }
