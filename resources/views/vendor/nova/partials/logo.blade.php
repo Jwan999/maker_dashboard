3f78bdc3d9cb1e2f75ceb77f2cbe8c338f58a8f0
Jwan999 <jwanaalfatla1999@gmail.com>
2021-12-09 12:52:13 +0300

    Added Fallujah Maker Space

--- resources/views/vendor/nova/partials/logo.blade.php
+++ resources/views/vendor/nova/partials/logo.blade.php
@@ -5,6 +5,9 @@ if (str_contains(url(''), 'iotkids')) {
 } elseif (str_contains(url(''), 'iotmaker')) {
     echo '<h1>IoT Maker</h1>';
     }
+elseif (str_contains(url(''), 'fallujahmakerspace')) {
+    echo '<h1>Fallujah</h1>';
+}
 else{
     echo 'You Not A Maker';
 }
