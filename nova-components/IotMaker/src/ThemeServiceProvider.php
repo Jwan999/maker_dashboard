0eaefe11ef5545dd1fec89295c9750ca0745454c
Jwan999 <jwanaalfatla1999@gmail.com>
2021-12-28 20:50:55 +0300

    Changed startups view

--- nova-components/IotMaker/src/ThemeServiceProvider.php
+++ nova-components/IotMaker/src/ThemeServiceProvider.php
@@ -21,7 +21,7 @@ class ThemeServiceProvider extends ServiceProvider
             } elseif (str_contains(url(''), 'iotmaker')) {
                 Nova::theme(asset('/css/iotmaker.css?v=1'));
             } elseif (str_contains(url(''), 'fallujahmakerspace')) {
-                Nova::theme(asset('/css/fallujah.css?v=1'));
+                Nova::theme(asset('/css/fallujah.css?v=2'));
             } elseif (str_contains(url(''), 'erbilmakerspace')) {
                 Nova::theme(asset('/css/erbil.css?v=2'));
             } else {
