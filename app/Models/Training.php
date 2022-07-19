1efab23409d1cac43f7ca4e5255f30db72261e58
Jwan999 <jwanaalfatla1999@gmail.com>
2022-07-19 12:32:34 +0300

    main

--- app/Models/Training.php
+++ app/Models/Training.php
@@ -4,10 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Laravel\Scout\Searchable;
+//use Laravel\Scout\Searchable;
 class Training extends Base
 {
-    use Searchable;
+//    use Searchable;
     use HasFactory;
 
     protected $with = ["students"];
